be1262c7cb086d068a1630ded7022af7d1930871
Kovalenko <vl.rtqw.py@gmail.com>
2023-09-18 12:43:24 +0300

    added booking model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import timedelta, date, datetime, time
 
 
 class GameMaster(models.Model):
@@ -20,3 +21,35 @@ class Game(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class BookingTime(models.Model):
+    user_phone = models.CharField('Телефон', max_length=255, blank=True, null=True)
+    game_system = models.ForeignKey(Game, on_delete=models.CASCADE, blank=True, null=True)
+    game_master = models.ForeignKey(GameMaster, on_delete=models.CASCADE, blank=True, null=True)
+    date = models.DateField('Дата')
+    start_time = models.TimeField('Начало')
+    end_time = models.TimeField('Конец')
+    is_available = models.BooleanField('Бронь', default=True)
+
+    @classmethod
+    def generate_bookings(cls):
+        start_date = date.today()
+        end_date = start_date + timedelta(days=21)
+
+        # Генерируем записи на пятницу, субботу и воскресенье
+        current_date = start_date
+        while current_date <= end_date:
+            if current_date.weekday() in [4, 5, 6]:  # Проверяем, является ли день пятницей, субботой или воскресеньем
+                # Проверяем наличие записей для каждого временного интервала
+                start_time = datetime.combine(current_date, time(hour=9))  # Начало работы
+                end_time = datetime.combine(current_date, time(hour=18))  # Конец работы
+
+                # Генерируем записи с интервалом в 1 час, если записей нет
+                while start_time < end_time:
+                    if not cls.objects.filter(date=current_date, start_time=start_time.time()).exists():
+                        cls.objects.create(date=current_date, start_time=start_time.time(),
+                                           end_time=(start_time + timedelta(hours=1)).time())
+                    start_time += timedelta(hours=1)
+
+            current_date += timedelta(days=1)
